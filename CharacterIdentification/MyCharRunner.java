d8c9fa8aadf935fa0424609ddf89df6589833ab2
Lavkush Singh <luvkushsynghh@gmail.com>
2020-05-11 23:55:03 +0530

    identifying the character entered, runner class

---
+++ CharacterIdentification/MyCharRunner.java
@@ -0,0 +1,24 @@
+package com.lavkush.CharacterIdentify;
+
+import java.util.Scanner;
+
+public class MyCharRunner {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the charater you wish to check: ");
+		char ch = scan.next().charAt(0);
+		scan.close();
+		
+		MyChar mychar = new MyChar(ch);
+		
+		System.out.print("\nYou have entered: "+ch);
+		System.out.println("\n\nIs this vowel: "+mychar.isVowel());
+		System.out.println("Is this consonent: "+mychar.isConsonent());
+		System.out.println("Is this Digit: "+mychar.isDigit());
+		System.out.println("Is this Alphabet: "+mychar.isAlhpabet());
+	}
+
+}
