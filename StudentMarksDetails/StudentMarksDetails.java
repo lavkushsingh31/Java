8dc79d1f6ca4e40845f6f6fd3637a02f49b62727
Lavkush Singh <luvkushsynghh@gmail.com>
2020-05-18 23:16:50 +0530

    Program to play withe details entered by Student

---
+++ StudentMarksDetails/StudentMarksDetails.java
@@ -0,0 +1,61 @@
+package com.lavkush.StudentMarksDetails;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
+
+public class StudentMarksDetails {
+
+	
+	private String name;
+	private int[] listOfMarks;
+
+	public StudentMarksDetails(String name, int[] listOfMarks) {
+		this.name = name;
+		this.listOfMarks = listOfMarks;
+	}
+
+	public int getTotalSumOfMarks() {
+		int sum=0;
+		for(int element:listOfMarks) {
+			sum = sum+element;
+		}
+		return sum;
+	}
+
+	public int getNumberOfMarks() {
+		
+		return listOfMarks.length;
+	}
+
+	public int getMaximumMark() {
+		int max = 0;
+		for(int element:listOfMarks) {
+			if(element > max) {
+				max = element;
+			}
+		}
+		return max;
+	}
+
+	public int getminimumMark() {
+		Arrays.sort(listOfMarks);
+		System.out.println(Arrays.toString(listOfMarks));
+		return listOfMarks[0];
+	}
+
+	public BigDecimal getAverageMarks() {
+		
+		return new BigDecimal(getTotalSumOfMarks()).divide( new BigDecimal( getNumberOfMarks() ), 3, RoundingMode.UP);
+		 
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int[] getListOfMarks() {
+		return listOfMarks;
+	}
+
+}
