340b00144b117f661dbd7ca2cad94a050da4f12d
Lavkush Singh <luvkushsynghh@gmail.com>
2020-05-18 23:12:47 +0530

    traversing array using enhanced for loop

---
+++ ArrayDemo/TraversingArray.java
@@ -0,0 +1,15 @@
+package com.lavkush.ArrayDemo;
+
+public class TraversingArray {
+
+	public static void main(String[] args) {
+		
+		int[] arrayDemo = {21,34,23,65,78,89};
+		
+		for(int element:arrayDemo) {
+			System.out.println(element);
+		}
+
+	}
+
+}
